0e5fbdf52dbfd175aa3f0f29ef737a86a509e7e0
Matthew Glezos <77373342+mglezos1@users.noreply.github.com>
2024-05-30 00:32:09 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import MintForm from './components/MintForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="page-heading">Toronto DAO Business Membership</h1>
+      <MintForm />
+    </div>
+  );
+}
+
+export default App;
